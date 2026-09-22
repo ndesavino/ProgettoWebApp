a93f84690537884329aae8983382f8194da63e87
gdshon <gabrieledib04@gmail.com>
2026-09-22 17:55:49 +0200

    creazione api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// Creazione di un'istanza base di Axios
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api', // L'URL del tuo backend Node.js
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+// Interceptor per aggiungere automaticamente il Token JWT ad ogni richiesta
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
