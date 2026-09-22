3ff9e5293a6925705d3de4bfb1dbcaf30b45f478
gdshon <gabrieledib04@gmail.com>
2026-09-22 17:57:01 +0200

    Creazione AuthContext.jsx

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useEffect } from 'react';
+import api from '../services/api';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    // Funzione asincrona per il login
+    const login = async (email, password) => {
+        try {
+            // Chiamata HTTP POST al backend
+            const response = await api.post('/auth/login', { email, password });
+            const { token, userData } = response.data; // Destrutturazione
+            
+            localStorage.setItem('token', token);
+            setUser(userData);
+        } catch (error) {
+            console.error("Errore di login", error);
+            throw error;
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
